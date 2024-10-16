d350fadaec3497f2ce312e21fca6d211e239eb92
wonkuji <wonkuji@polarisoffice.com>
2024-10-16 18:05:13 +0900

    초기 소스 commit

---
+++ LetsTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsTogether
+//
+//  Created by CNT-20-00025 on 10/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LetsTogetherUITests/LetsTogetherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LetsTogetherUITestsLaunchTests.swift
+//  LetsTogetherUITests
+//
+//  Created by CNT-20-00025 on 10/16/24.
+//
+
+import XCTest
+
+final class LetsTogetherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
